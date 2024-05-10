fbf22d6bd79ca112a28cb9e4edbc2ee1458f6a08
Mitesh Jain <miteshjain2808@gmail.com>
2024-05-10 19:09:56 +0530

    added DCN Lab2

---
+++ DCN/Lab2/Server.java
@@ -0,0 +1,28 @@
+package DCN.Lab2;
+
+import java.net.*;
+import java.io.*;
+
+public class Server {
+    public static void main(String[] args) throws Exception {
+        ServerSocket sersock = new ServerSocket(4000);
+        System.out.println("Server ready for connection");
+        Socket sock = sersock.accept();
+        System.out.println("Connection Is successful and waiting for chatting");
+        InputStream istream = sock.getInputStream();
+        BufferedReader fileRead = new BufferedReader(new InputStreamReader(istream));
+        String fname = fileRead.readLine();
+        BufferedReader ContentRead = new BufferedReader(new FileReader(fname));
+        OutputStream ostream = sock.getOutputStream();
+        PrintWriter pwrite = new PrintWriter(ostream, true);
+        String str;
+        while ((str = ContentRead.readLine()) != null) {
+            pwrite.println(str);
+        }
+        sock.close();
+        sersock.close();
+        pwrite.close();
+        fileRead.close();
+        ContentRead.close();
+    }
+}
