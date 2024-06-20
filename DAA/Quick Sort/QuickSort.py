ccc66d9d84639e042d9806fcf18b851a0c12e69d
Mitesh Jain <miteshjain2808@gmail.com>
2024-06-20 06:19:00 +0530

    updated quicksort

--- DAA/Quick Sort/QuickSort.py
+++ DAA/Quick Sort/QuickSort.py
@@ -11,8 +11,8 @@ def partition(array, low, high):
 def quickSort(array, low, high):
     if low < high:
         pi = partition(array, low, high)
-        quickSort(array, low, pi - 1)
-        quickSort(array, pi + 1, high)
+        quickSort(array, low, pi-1)
+        quickSort(array, pi+1, high)
 
 arr = [64, 25, 12, 22, 11] 
 quickSort(arr, 0, len(arr)-1) 
