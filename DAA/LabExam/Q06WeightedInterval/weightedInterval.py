29dfe3413801bf91b7a3fafae4f0e2efa1288cd2
Mitesh Jain <miteshjain2808@gmail.com>
2024-07-18 06:13:08 +0530

    updated weightedInterval

--- DAA/LabExam/Q06WeightedInterval/weightedInterval.py
+++ DAA/LabExam/Q06WeightedInterval/weightedInterval.py
@@ -1,40 +1,40 @@
-def ComputeOpt(x):
-    if x == 0:
+def ComputeOpt(j):
+    if j < 0:
         return 0
-    elif M[x - 1]:
-        return M[x - 1]
-    else:
-        M[x - 1] = max(ComputeOpt(x - 1), lst[x - 1][2] + ComputeOpt(p[x - 1]))
-        return M[x - 1]
-
+    if M[j]:
+        return M[j]
+    M[j] = max(ComputeOpt(j - 1), lst[j][2] + ComputeOpt(p[j]))
+    return M[j]
 
 def findSolution(k):
-    if k < 1:
+    if k < 0:
         return
-    i = k
-    while i > 1 and M[i - 1] == M[i - 2]:
-        i -= 1
-    selected.append(lst[i - 1])
-    findSolution(p[i - 1])
-
+    if k == 0 or M[k] != M[k - 1]:
+        selected.append(lst[k])
+        findSolution(p[k])
+    else:
+        findSolution(k - 1)
 
 n = int(input("Enter number of events: "))
 lst = []
-print("Enter start time, finish time and profit of each event separated by space")
+print("Enter start time, finish time, and profit of each event separated by space")
 for i in range(n):
-    tup = tuple(map(int, input(f"Schedule {i+1}: ").split()))
+    tup = tuple(map(int, input(f"Schedule {i + 1}: ").split()))
     lst.append(tup)
+
 lst = sorted(lst, key=lambda x: x[1])
 
-p = [0 for i in range(len(lst))]
-for i in range(len(lst) - 1, 0, -1):
+p = [0] * len(lst)
+for i in range(len(lst)):
     for j in range(i - 1, -1, -1):
         if lst[i][0] >= lst[j][1]:
-            p[i] = j + 1
+            p[i] = j
             break
+    else:
+        p[i] = -1
 
-M = [0 for i in range(len(lst))]
+M = [0] * len(lst)
 selected = []
-print("\nMax profit: ",ComputeOpt(len(lst)))
-findSolution(len(lst))
-print("\nEvents selected: ",selected)
+print("\nMax profit:", ComputeOpt(len(lst) - 1))
+findSolution(len(lst) - 1)
+print("\nEvents selected:", selected)
