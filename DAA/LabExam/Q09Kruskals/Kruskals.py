47eba275810a47b29bfd2a793344b58aaf86514e
Mitesh Jain <miteshjain2808@gmail.com>
2024-08-04 18:08:40 +0530

    simplified Kruskal's code

--- DAA/LabExam/Q09Kruskals/Kruskals.py
+++ DAA/LabExam/Q09Kruskals/Kruskals.py
@@ -3,20 +3,10 @@ def find(parent, i):
         parent[i] = find(parent, parent[i]) 
     return parent[i]
 
-def union(parent, rank, x, y): 
-    if rank[x] < rank[y]: 
-        parent[x] = parent[y] 
-    elif rank[x] > rank[y]: 
-        parent[y] = parent[x] 
-    else: 
-        parent[y] = parent[x] 
-        rank[x] += 1
-
 def Kruskals(graph, V):
     mst = []
     graph = sorted(graph, key=lambda item: item[2])
     parent = [i for i in range(V)]
-    rank = [0 for i in range(V)]
     cost, edges, i = 0, 0, 0
     while edges < V-1:
         u, v, weight = graph[i]
@@ -25,14 +15,12 @@ def Kruskals(graph, V):
         if x != y:
             edges += 1
             mst.append([u, v, weight])
-            union(parent, rank, x, y)
+            parent[y] = parent[x]
             cost += weight
     print("\nMinimum Spanning Tree: ", mst)
     print("\nMinimum Spanning Tree Cost: ", cost)
 
 V = int(input("\nEnter no of vertices: "))
-parent = [i for i in range(V)]
-rank = [0 for i in range(V)]
 graph = []
 E = int(input("\nEnter the no of edges: "))
 print("Enter edges and their weight separated by space(u v weight)")
