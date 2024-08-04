c2f2f52a043379180c6a11a876ec8126409db6b1
Mitesh Jain <miteshjain2808@gmail.com>
2024-08-04 15:07:50 +0530

    DAA final except TSP

--- DAA/LabExam/Q11SubsetSum/SubsetSum.py
+++ DAA/LabExam/Q11SubsetSum/SubsetSum.py
@@ -1,20 +1,35 @@
-def subsetSum(n, W, weights):
+def subsetSum(weights, W, n):
     M = [[False] * (W + 1) for _ in range(n + 1)]
     for i in range(n + 1):
         M[i][0] = True
     for i in range(1, n + 1):
         for w in range(1, W + 1):
-            if weights[i-1] > w:
+            if w < weights[i-1]:
                 M[i][w] = M[i-1][w]
             else:
                 M[i][w] = M[i-1][w] or M[i-1][w - weights[i-1]]
-    return M[n][W]
+    return M
+
+def findSubsets(weights, W, n, M):
+    if not M[n][W]:
+        return None  # No subset with given sum
+    selected_items = []
+    w = W
+    for i in range(n, 0, -1):
+        if M[i][w] and not M[i - 1][w]:
+            selected_items.append(weights[i - 1])
+            w -= weights[i - 1]
+    return selected_items
 
 n = int(input("Enter the number of elements: "))
 weights = [int(input(f"Enter weight {i+1}: ")) for i in range(n)]
 W = int(input("Enter the target sum: "))
 
-if subsetSum(n, W, weights):
+M = subsetSum(weights, W, n)
+
+if M[n][W]:
     print(f"A subset with sum {W} exists.")
+    subset = findSubsets(weights, W, n, M)
+    print(f"Subset that sums to {W} is:",subset)
 else:
     print(f"No subset with sum {W} exists.")
--- DAA/LabExam/Q12Knapsack/Knapsack.py
+++ DAA/LabExam/Q12Knapsack/Knapsack.py
@@ -1,19 +1,19 @@
-def knapsack(items, maxWeight, n):
-    dp = [[0] * (maxWeight + 1) for i in range(n + 1)]
+def knapsack(items, W, n):
+    dp = [[0] * (W + 1) for i in range(n + 1)]
     for i in range(1, n + 1):
-        for w in range(maxWeight + 1):
+        for w in range(W + 1):
             if items[i-1][1] <= w:
                 dp[i][w] = max(dp[i-1][w], dp[i-1][w - items[i-1][1]] + items[i-1][2])
             else:
                 dp[i][w] = dp[i-1][w]
-    w = maxWeight
+    w = W
     selected = []
     for i in range(n, 0, -1):
         if dp[i][w] != dp[i-1][w]:
             selected.append(items[i-1])
             w -= items[i-1][1]
     selected.reverse()
-    return dp[n][maxWeight], selected
+    return dp[n][W], selected
 
 n = int(input("Enter number of items: "))
 items = []
@@ -23,8 +23,8 @@ for i in range(n):
     weight, value = map(int, input(f"Item {i+1}: ").split())
     items.append((i+1, weight, value))
 
-maxWeight = int(input("Enter maximum weight: "))
+W = int(input("Enter maximum weight: "))
 
-maxValue, selected = knapsack(items, maxWeight, n)
+maxValue, selected = knapsack(items, W, n)
 print("Maximum Value:", maxValue)
 print("Selected Items:", selected)
